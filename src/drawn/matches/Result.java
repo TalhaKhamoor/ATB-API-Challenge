5dc232d0715cabffb67992f18f5a89ba782a5ec0
Talha Khamoor <talhak597@gmail.com>
2021-10-07 09:49:22 -0600

    connect to url and get 200 status

--- src/drawn/matches/Result.java
+++ src/drawn/matches/Result.java
@@ -1,16 +1,31 @@
 package drawn.matches;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class Result {
 
 	/*
-     * Complete the 'getNumDraws' function below.
-     *
-     * The function is expected to return an INTEGER.
-     * The function accepts INTEGER year as a parameter.
-     */
+	 * Complete the 'getNumDraws' function below.
+	 *
+	 * The function is expected to return an INTEGER. The function accepts INTEGER
+	 * year as a parameter.
+	 */
 
-    public static int getNumDraws(int year) {
-		return year;
-        // Your code here
-    }
+	public static int getNumDraws(int year) throws IOException {
+		HttpURLConnection connection = null;
+		String urlString = "https://jsonmock.hackerrank.com/api/football_matches?year=" + year;
+
+		URL url = new URL(urlString);
+		connection = (HttpURLConnection) url.openConnection();
+
+		connection.setRequestMethod("GET");
+		connection.setConnectTimeout(5000);
+		connection.setReadTimeout(5000);
+
+		System.out.println(connection.getResponseCode());
+		return 0;
+	}
 }
