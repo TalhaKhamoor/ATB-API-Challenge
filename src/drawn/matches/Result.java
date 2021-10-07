66d934d9c922fa4afa55dfa1021fb2ded9387c5b
Talha Khamoor <talhak597@gmail.com>
2021-10-07 10:00:10 -0600

    GET json string from API

--- src/drawn/matches/Result.java
+++ src/drawn/matches/Result.java
@@ -1,6 +1,8 @@
 package drawn.matches;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -24,8 +26,28 @@ public class Result {
 		connection.setRequestMethod("GET");
 		connection.setConnectTimeout(5000);
 		connection.setReadTimeout(5000);
+		
+		BufferedReader reader;
+		StringBuffer responseContent = new StringBuffer();
+		String line;
 
-		System.out.println(connection.getResponseCode());
+		if (connection.getResponseCode() > 299) {
+			reader = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
+			while ((line = reader.readLine()) != null) {
+				responseContent.append(line + "\n");
+			}
+			reader.close();
+
+		} else {
+			reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+			while ((line = reader.readLine()) != null) {
+				responseContent.append(line);
+			}
+			reader.close();
+
+		}
+		
+		System.out.println(responseContent.toString());
 		return 0;
 	}
 }
