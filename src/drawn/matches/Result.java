7a2ee59bcfbb1fa8beaa0579b21ef587b1a4b034
Talha Khamoor <talhak597@gmail.com>
2021-10-07 09:42:52 -0600

    set given code

---
+++ src/drawn/matches/Result.java
@@ -0,0 +1,16 @@
+package drawn.matches;
+
+public class Result {
+
+	/*
+     * Complete the 'getNumDraws' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER year as a parameter.
+     */
+
+    public static int getNumDraws(int year) {
+		return year;
+        // Your code here
+    }
+}
---
+++ src/drawn/matches/Solution.java
@@ -0,0 +1,26 @@
+package drawn.matches;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Solution {
+
+	public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int year = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int result = Result.getNumDraws(year);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+
+}
